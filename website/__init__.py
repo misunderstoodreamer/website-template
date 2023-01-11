7378d608b05b8bb2854fd7f1d2047724dfe42ba6
misunderstoodreamer <yagmur16@itu.edu.tr>
2023-01-12 02:19:03 +0300

    backend template completed

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -13,6 +12,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+
+
     from .views import views
     from .auth import auth
 
@@ -24,6 +25,14 @@ def create_app():
     with app.app_context():
         db.create_all()
         print("db created_")
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
---
+++ website/static/index.js
@@ -0,0 +1,8 @@
+function delete_note(noteId){
+    fetch('/delete-note', {
+        method: 'POST',
+        body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+        window.location.href = '/'
+    });
+}
